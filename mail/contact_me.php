eeaa983742348a0317a0072d2906520f27346bbb
neff386 <day900@aol.com>
2017-12-18 14:29:47 -0500

    sent up send grid email

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,4 +1,6 @@
 <?php
+//
+require 'vendor/autoload.php';
 // Check for empty fields
 if(empty($_POST['name'])      ||
    empty($_POST['email'])     ||
@@ -16,11 +18,23 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'dturner2341@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$from = new SendGrid\Email("Royal Weddings", "NOREPLAY@cjroyalwed.com");
+$to = new SendGrid\Email("David Turner","dturner2341@gmail.com"); // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$email_body = new SendGrid\Content("You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message");
 $headers = "From: noreply@royalweddings.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
-mail($to,$email_subject,$email_body,$headers);
+$headers .= "Reply-To: $email_address";  
+$mail = SendGrid\Mail($from, $headers, $to, $email_body); 
+
+$apiKey = getenv('SENDGRID_API_KEY');
+$sg = new \SendGrid($apiKey);
+
+$response = $sg->client->mail()->send()->post($mail);
+echo $response->statusCode();
+print_r($response->headers());
+echo $response->body();
+
+
+//mail($to,$email_subject,$email_body,$headers);
 return true;         
 ?>
